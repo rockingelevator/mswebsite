dad6771cae4269de6aa1e943f089f0c8b7b06385
Kirill Meshkov <rockingelevator@gmail.com>
2018-06-02 13:58:36 +0300

    fix

--- static/scripts/particles.js
+++ static/scripts/particles.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', e => {
+  console.log('load event');
   particlesJS.load('particles-js', `/scripts/particles.json`, () => {
     console.log('callback - particles.js config loaded');
   });
