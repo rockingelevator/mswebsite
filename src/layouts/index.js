fc9c28331b8620ca858bc20d39e31b89071df92e
Kirill Meshkov <rockingelevator@gmail.com>
2018-06-02 23:21:12 +0300

    favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -66,7 +66,28 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { name: 'msapplication-TileColor', content: '#eeeeee' },
+        { name: 'theme-color', content: '#ffffff' },
       ]}>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
       <script src={withPrefix('/scripts/particles-lib.js')} />
       <script src={withPrefix('/scripts/particles.js')} />
     </Helmet>
